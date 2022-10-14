352eb6cbeb389325acc7654a365f0976461d6d25
vitoria <vitoria@lepidus.com.br>
2022-10-14 15:56:46 -0300

    remove unuseful comments
    issue: documentacao-e-tarefas/scielo#390
    
    Signed-off-by: vitoria <vitoria@lepidus.com.br>
    Signed-off-by: jhon <jhon@lepidus.com.br>

--- PlauditPreEndorsementPlugin.inc.php
+++ PlauditPreEndorsementPlugin.inc.php
@@ -25,13 +25,10 @@ class PlauditPreEndorsementPlugin extends GenericPlugin
 
         if ($success && $this->getEnabled($mainContextId)) {
             HookRegistry::register('Templates::Submission::SubmissionMetadataForm::AdditionalMetadata', array($this, 'addEndorserFieldToStep3'));
-            
+
             HookRegistry::register('submissionsubmitstep3form::readuservars', array($this, 'allowStep3FormToReadOurFields'));
             HookRegistry::register('submissionsubmitstep3form::execute', array($this, 'step3SaveOurFieldsInDatabase'));
             HookRegistry::register('Schema::get::publication', array($this, 'addOurFieldsToPublicationSchema'));
-            
-            //dar uma olhada se precisa mesmo:
-            //HookRegistry::register('SubmissionHandler::saveSubmit', array($this, 'passOurFieldsValuesToSubmission'));
         }
 
         return $success;
@@ -58,7 +55,7 @@ class PlauditPreEndorsementPlugin extends GenericPlugin
 
     public function allowStep3FormToReadOurFields($hookName, $params)
     {
-        $formFields =& $params[1];
+        $formFields = &$params[1];
         $ourFields = ['endorserEmail'];
 
         $formFields = array_merge($formFields, $ourFields);
@@ -69,7 +66,7 @@ class PlauditPreEndorsementPlugin extends GenericPlugin
         $step3Form = $params[0];
         $publication = $step3Form->submission->getCurrentPublication();
         $endorserEmail = $step3Form->getData('endorserEmail');
-        
+
         $publication->setData('endorserEmail', $endorserEmail);
         $publicationDao = DAORegistry::getDAO('PublicationDAO');
         $publicationDao->updateObject($publication);
@@ -77,7 +74,7 @@ class PlauditPreEndorsementPlugin extends GenericPlugin
 
     public function addOurFieldsToPublicationSchema($hookName, $params)
     {
-        $schema =& $params[0];
+        $schema = &$params[0];
 
         $schema->properties->{'endorserEmail'} = (object) [
             'type' => 'string',
