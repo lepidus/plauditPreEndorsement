a6fca3337c9e87c8df43e887b1569bbb1d30625a
Jhon <jhon@lepidus.com.br>
2022-11-23 16:26:35 -0400

    Adds tests for plaudit API response handling
    Issue: documentacao-e-tarefas/scielo#412
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>
    Signed-off-by: Richard <richard@lepidus.com.br>

---
+++ tests/PlauditClientTest.php
@@ -0,0 +1,58 @@
+<?php
+
+import('classes.publication.Publication');
+import('plugins.generic.plauditPreEndorsement.classes.PlauditClient');
+import('plugins.generic.plauditPreEndorsement.tests.TestResponse');
+import('plugins.generic.plauditPreEndorsement.PlauditPreEndorsementPlugin');
+
+use PHPUnit\Framework\TestCase;
+
+final class PlauditClientTest extends TestCase
+{
+    private $publication;
+    private $plauditClient;
+    private $doi = '10.1590/LepidusPreprints.1535';
+    private $orcid = '0000-0001-5542-5100';
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->publication = new Publication();
+        $this->publication->setData('pub-id::doi', $this->doi);
+        $this->publication->setData('endorserOrcid', $this->orcid);
+        
+        $this->plauditClient = new PlauditClient();
+    }
+
+    public function testEndorsementStatusWhenRequestSucceed(): void
+    {
+        $statusOk = 200;
+        $bodyJson = "{\"endorsements\":[{\"doi\":\"$this->doi\"}\",\"orcid\":\"$this->orcid\",\"tags\":[]}]}";
+        $response = new TestResponse($statusOk, $bodyJson);
+        
+        $this->assertEquals(ENDORSEMENT_STATUS_COMPLETED, $this->plauditClient->getEndorsementStatusByResponse($response, $this->publication));
+    }
+
+    public function testEndorsementStatusWhenRequestSucceedButDataDiffs(): void
+    {
+        $statusOk = 200;
+        $bodyJson = "{\"endorsements\":[{\"doi\":\"10.1590/LepidusPreprints.2022\"}\",\"orcid\":\"$this->orcid\",\"tags\":[]}]}";
+        $response = new TestResponse($statusOk, $bodyJson);
+
+        $this->assertEquals(ENDORSEMENT_STATUS_COULDNT_COMPLETE, $this->plauditClient->getEndorsementStatusByResponse($response, $this->publication));
+
+        $bodyJson = "{\"endorsements\":[{\"doi\":\"$this->doi\"}\",\"orcid\":\"0000-0001-5542-1234\",\"tags\":[]}]}";
+        $response = new TestResponse($statusOk, $bodyJson);
+        
+        $this->assertEquals(ENDORSEMENT_STATUS_COULDNT_COMPLETE, $this->plauditClient->getEndorsementStatusByResponse($response, $this->publication));
+    }
+
+    public function testEndorsementStatusWhenRequestFails(): void
+    {
+        $statusBadRequest = 400;
+        $bodyJson = "";
+        $response = new TestResponse($statusBadRequest, $bodyJson);
+        
+        $this->assertEquals(ENDORSEMENT_STATUS_COULDNT_COMPLETE, $this->plauditClient->getEndorsementStatusByResponse($response, $this->publication));
+    }
+}
---
+++ tests/TestResponse.inc.php
@@ -0,0 +1,25 @@
+<?php
+
+class TestResponse {
+
+    private $statusCode;
+    private $body;
+
+    public function __construct($statusCode, $body)
+    {
+        $this->statusCode = $statusCode;
+        $this->body = $body;
+    }
+
+    public function getStatusCode()
+    {
+        return $this->statusCode;
+    }
+
+    public function getBody()
+    {
+        return $this->body;
+    }
+}
+
+?>
