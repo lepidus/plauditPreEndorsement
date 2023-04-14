5cf39bb88fb7cd722efb04a30048e34755af99ce
Jhon <jhon@lepidus.com.br>
2023-04-14 14:57:39 -0400

    Updates message of endorsement sending error written to log
    Issue: documentacao-e-tarefas/scielo#514
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>
    Signed-off-by: Lais <lais@lepidus.com.br>

--- PlauditPreEndorsementPlugin.inc.php
+++ PlauditPreEndorsementPlugin.inc.php
@@ -240,8 +240,10 @@ class PlauditPreEndorsementPlugin extends GenericPlugin
                 $response = $plauditClient->requestEndorsementCreation($publication, $secretKey);
                 $newEndorsementStatus = $plauditClient->getEndorsementStatusByResponse($response, $publication);
             } catch (ClientException $exception) {
-                $reason = print_r($exception->getResponse()->getBody(false), true);
-                $this->writeOnActivityLog($submission, 'plugins.generic.plauditPreEndorsement.log.failedSendingEndorsement', ['reason' => $reason]);
+                $response = $exception->getResponse();
+                $responseCode = $response->getStatusCode();
+                $responseBody = print_r($response->getBody()->getContents(), true);
+                $this->writeOnActivityLog($submission, 'plugins.generic.plauditPreEndorsement.log.failedSendingEndorsement', ['code' => $responseCode, 'body' => $responseBody]);
                 $newEndorsementStatus = ENDORSEMENT_STATUS_COULDNT_COMPLETE;
             }
 
