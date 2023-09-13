a030cfb0bd58dce26c03e576cc03f85f93233714
Jhon <jhon@lepidus.com.br>
2023-09-13 16:40:35 -0400

    Creates ORCID client class
    Issue: documentacao-e-tarefas/scielo#589
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

---
+++ tests/OrcidClientTest.php
@@ -0,0 +1,42 @@
+<?php
+
+import('plugins.generic.plauditPreEndorsement.classes.OrcidClient');
+import('plugins.generic.plauditPreEndorsement.PlauditPreEndorsementPlugin');
+
+use PHPUnit\Framework\TestCase;
+
+final class OrcidClientTest extends TestCase
+{
+    private $testRecord = [
+        'person' => [
+            'last-modified-date' => '',
+            'name' => [
+                'created-date' => [
+                    'value' => 1666816304613
+                ],
+                'last-modified-date' => [
+                    'value' => 1666816304613
+                ],
+                'given-names' => [
+                    'value' => 'Alfred'
+                ],
+                'family-name' => [
+                    'value' => 'Hitchcock'
+                ],
+                'credit-name' => '',
+                'source' => '',
+                'visibility' => 'public',
+                'path' => '0000-0001-5542-5100'
+            ]
+        ]
+    ];
+
+    public function testGetFullNameFromRecord(): void
+    {
+        $plugin = new PlauditPreEndorsementPlugin();
+        $contextId = 1;
+        $orcidClient = new OrcidClient($plugin, $contextId);
+
+        $this->assertEquals('Alfred Hitchcock', $orcidClient->getFullNameFromRecord($this->testRecord));
+    }
+}
