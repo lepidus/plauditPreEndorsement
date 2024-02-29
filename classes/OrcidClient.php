f7762d40cf7ff91c9572f1e2ff9cfe225ce4b79c
Jhon <jhon@lepidus.com.br>
2024-02-29 15:42:38 -0400

    Finishes adapting of handler class
    Issue: documentacao-e-tarefas/scielo#635
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- classes/OrcidClient.php
+++ classes/OrcidClient.php
@@ -6,6 +6,15 @@ use APP\core\Application;
 
 class OrcidClient
 {
+    public const ORCID_URL = 'https://orcid.org/';
+    public const ORCID_URL_SANDBOX = 'https://sandbox.orcid.org/';
+    public const ORCID_API_URL_PUBLIC = 'https://pub.orcid.org/';
+    public const ORCID_API_URL_PUBLIC_SANDBOX = 'https://pub.sandbox.orcid.org/';
+    public const ORCID_API_URL_MEMBER = 'https://api.orcid.org/';
+    public const ORCID_API_URL_MEMBER_SANDBOX = 'https://api.sandbox.orcid.org/';
+    public const ORCID_API_SCOPE_PUBLIC = '/authenticate';
+    public const ORCID_API_SCOPE_MEMBER = '/activities/update';
+    
     private $plugin;
     private $contextId;
 
