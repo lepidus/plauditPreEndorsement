88d5cb706019455154dcaae6ba60dec08795b919
Jhon <jhon@lepidus.com.br>
2024-03-01 14:32:02 -0400

    Updates integration test to check for endorsement validation
    Issue: documentacao-e-tarefas/scielo#635
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- cypress/tests/Test1_endorserFieldsSubmission.cy.js
+++ cypress/tests/Test1_endorserFieldsSubmission.cy.js
@@ -1,3 +1,5 @@
+import '../support/commands.js';
+
 function beginSubmission(submissionData) {
     cy.get('input[name="locale"][value="en"]').click();
     cy.setTinyMceContent('startSubmission-title-control', submissionData.title);
@@ -10,8 +12,7 @@ function beginSubmission(submissionData) {
 describe("Plaudit Pre-Endorsement Plugin - Endorser fields in submission wizard", function() {
     let submissionData;
     let dummyPdf;
-    let endorserName = 'Bong Joon-ho';
-    let endorserEmail = 'bong.joon-ho@email.kr';
+    let endorsers;
     
     before(function() {
         Cypress.config('defaultCommandTimeout', 4000);
@@ -25,25 +26,65 @@ describe("Plaudit Pre-Endorsement Plugin - Endorser fields in submission wizard"
             'mimeType': 'application/pdf',
             'genre': 'Preprint Text'
         };
+        endorsers = {
+            invalidEmail: {
+                name: 'John Wayne',
+                email: 'john.wayne.email'
+            },
+            isAuthor: {
+                name: 'Catherine Kwantes',
+                email: 'ckwantes@mailinator.com'
+            },
+            correct: {
+                name: 'Bong Joon-ho',
+                email: 'bong.joon-ho@email.kr'
+            }
+        };
     });
     
-    it("Author user submits endorsed submission", function() {
+    it("Checks endorsement fields and validates endorser email", function() {
         cy.login('ckwantes', null, 'publicknowledge');
         cy.get('div#myQueue a:contains("New Submission")').click();
 
         beginSubmission(submissionData);
-        
         cy.setTinyMceContent('titleAbstract-abstract-control-en', submissionData.abstract);
+        
         cy.contains('h2', 'Endorsement');
         cy.contains('Do you have the endorsement of an experienced researcher in the field of knowledge of the manuscript?');
         cy.contains('If yes, please provide the name and e-mail address of the endorsing researcher. Endorsements can significantly speed up the moderation process.');
         cy.contains('The endorsement cannot be given by one of the authors of the manuscript.');
 
-        cy.get('input[name="endorserName"]').clear().type(endorserName, {delay: 0});
-        cy.get('input[name="endorserEmail"]').clear().type(endorserEmail, {delay: 0});
+        cy.get('input[name="endorserName"]').clear().type(endorsers.invalidEmail.name, {delay: 0});
+        cy.get('input[name="endorserEmail"]').clear().type(endorsers.invalidEmail.email, {delay: 0});
         cy.contains('button', 'Continue').click();
         
         cy.addSubmissionGalleys([dummyPdf]);
+        cy.contains('button', 'Continue').click();
+        cy.contains('button', 'Continue').click();
+        cy.contains('button', 'Continue').click();
+        
+        cy.contains('Please enter a valid endorser e-mail');
+    });
+    it("Validates endorser is not an author of the submission", function() {
+        cy.login('ckwantes', null, 'publicknowledge');
+        cy.findSubmission('myQueue', submissionData.title);
+        
+        cy.get('input[name="endorserName"]').clear().type(endorsers.isAuthor.name, {delay: 0});
+        cy.get('input[name="endorserEmail"]').clear().type(endorsers.isAuthor.email, {delay: 0});
+        cy.contains('button', 'Continue').click();
+        cy.contains('button', 'Continue').click();
+        cy.contains('button', 'Continue').click();
+        cy.contains('button', 'Continue').click();
+        
+        cy.contains('The endorsement cannot be given by any of the authors of the manuscript');
+    });
+    it("Finishes submission with correct endorsement", function() {
+        cy.login('ckwantes', null, 'publicknowledge');
+        cy.findSubmission('myQueue', submissionData.title);
+        
+        cy.get('input[name="endorserName"]').clear().type(endorsers.correct.name, {delay: 0});
+        cy.get('input[name="endorserEmail"]').clear().type(endorsers.correct.email, {delay: 0});
+        cy.contains('button', 'Continue').click();
         cy.contains('button', 'Continue').click();
         cy.contains('button', 'Continue').click();
         cy.contains('button', 'Continue').click();
