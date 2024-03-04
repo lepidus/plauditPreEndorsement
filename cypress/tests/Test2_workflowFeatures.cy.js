fb70120f51ca8a2f46f35bf67ad7adf0580bce4f
Jhon <jhon@lepidus.com.br>
2024-03-04 15:03:28 -0400

    Refactors integration test
    Issue: documentacao-e-tarefas/scielo#635
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- cypress/tests/Test2_workflowFeatures.cy.js
+++ cypress/tests/Test2_workflowFeatures.cy.js
@@ -3,7 +3,18 @@ import '../support/commands.js';
 describe("Plaudit Pre-Endorsement Plugin - Workflow features", function() {
     let submissionTitle = "Killers of the Flower Moon";
 
-    it("E-mail sendings counting message in submission workflow", function() {
+    it("Endorsement information in workflow tab", function() {
+        cy.login('ckwantes', null, 'publicknowledge');
+        cy.findSubmission('myQueue', submissionTitle);
+        
+        cy.get("#publication-button").click();
+        cy.contains("Pre-Endorsement").click();
+        cy.get('input[name="endorserNameWorkflow"]').should('have.value', 'Bong Joon-ho');
+        cy.get('input[name="endorserEmailWorkflow"]').should('have.value', 'bong.joon-ho@email.kr');
+        cy.contains('The endorsement has not yet been confirmed by the endorser');
+        cy.contains("1 endorsement confirmation e-mail has been sent to the endorser");
+    });
+    it("E-mail sendings counting in workflow tab", function() {
         cy.login('ckwantes', null, 'publicknowledge');
         cy.findSubmission('myQueue', submissionTitle);
         
@@ -13,16 +24,7 @@ describe("Plaudit Pre-Endorsement Plugin - Workflow features", function() {
         cy.get("#plauditPreEndorsement").contains("Save").click();
         
         cy.reload();
-        cy.get("#publication-button").click();
-        cy.contains("Pre-Endorsement").click();
         cy.contains("2 endorsement confirmation e-mails have been sent to the endorser");
-    });
-    it("E-mail sendings count set to zero when endorser is changed", function() {
-        cy.login('ckwantes', null, 'publicknowledge');
-        cy.findSubmission('myQueue', submissionTitle);
-        
-        cy.get("#publication-button").click();
-        cy.contains("Pre-Endorsement").click();
         cy.get('input[name="endorserNameWorkflow"]').clear().type("Lady Diana", { delay: 0 });
         cy.get('input[name="endorserEmailWorkflow"]').clear().type("lady.diana@gmail.com", { delay: 0 });
         cy.get("#plauditPreEndorsement").contains("Save").click();
