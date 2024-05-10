7ba888e4a275b5602845cac80f1a0cfbf86dc95d
yves <yves@lepidus.com.br>
2024-05-10 15:33:08 -0400

    Implements Endorser email retrieval.
    Issue: documentacao-e-tarefas/scielo#656
    
    Signed-off-by: Yves César <yves@lepidus.com.br>

--- classes/Endorser.php
+++ classes/Endorser.php
@@ -5,10 +5,12 @@ namespace APP\plugins\generic\plauditPreEndorsement\classes;
 class Endorser
 {
     private $name;
+    private $email;
 
-    public function __construct(string $name)
+    public function __construct(string $name, string $email)
     {
         $this->name = $name;
+        $this->email = $email;
     }
 
     public function getName(): string
@@ -19,5 +21,10 @@ class Endorser
     public function setName(string $name)
     {
         $this->name = $name;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
     }
 }
--- tests/EndorserTest.php
+++ tests/EndorserTest.php
@@ -9,15 +9,21 @@ class EndorserTest extends TestCase
 {
     public function testEndorserNameRetrieval()
     {
-        $endorser = new Endorser("DummyEndorser");
+        $endorser = new Endorser("DummyEndorser", "dummy@mailinator.com");
         $this->assertEquals($endorser->getName(), "DummyEndorser");
     }
 
     public function testChangeEndorserName()
     {
-        $endorser = new Endorser("DummyEndorser");
+        $endorser = new Endorser("DummyEndorser", "dummy@mailinator.com");
         $this->assertEquals($endorser->getName(), "DummyEndorser");
         $endorser->setName("Dummy Hugo De León");
         $this->assertEquals($endorser->getName(), "Dummy Hugo De León");
+    }
+
+    public function testEndorserEmailRetrieval()
+    {
+        $endorser = new Endorser("DummyEndorser", "dummy@mailinator.com");
+        $this->assertEquals($endorser->getEmail(), "dummy@mailinator.com");
     }
 }
